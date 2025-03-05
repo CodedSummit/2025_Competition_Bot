3385dff170322d83323f3ef4713d25b57601e1c0
Sam Curren <telegramsam@gmail.com>
2025-03-05 06:59:37 -0700

    Arm quick cleanup. Note the use of MathUtil.clamp() for restricting the lower and upper bounds of a value.

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -8,10 +8,10 @@ import java.util.Map;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.GenericEntry;
@@ -22,31 +22,19 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Constants;
 import frc.robot.Constants.ArmConstants;
 
 @Logged
 public class ArmSubsystem extends SubsystemBase {
 
-  private SparkMaxConfig config= new SparkMaxConfig();
+  private SparkMaxConfig config = new SparkMaxConfig();
   private final DigitalInput inputElbow = new DigitalInput(7);
-
   private final DutyCycleEncoder absEncoder = new DutyCycleEncoder(inputElbow, -1.0, -Constants.ArmConstants.kElbowOffset);
   private final SparkMax m_elbow = new SparkMax(6, MotorType.kBrushless);
-  private boolean maxLimitReached = false;
-  private boolean minLimitReached = false;
-  //private double m_elbowSpeed = 0.2;
-  //private double m_elbowUpSpeed = Constd;
-  //private double m_elbowDownSpeed = Constants.ArmConstants.kElbowDownSpeed;  // make vars to allow tuning
-  
+
   private double m_elbowDesiredAngleDeg = 0.0;  // Angle we want the arm.  0.0 is horizontal, 90 straight up
 
 
@@ -55,14 +43,11 @@ public class ArmSubsystem extends SubsystemBase {
       ArmConstants.kSVolts, ArmConstants.kGVolts,
       ArmConstants.kVVoltSecondPerRad);
   private PIDController m_elbowPIDController = new PIDController(ArmConstants.kP,0,0);
-  private double m_handlerSpeed = ArmConstants.kHandlerDefaultSpeed;
-  private GenericEntry nt_elbowSpeed;
   private GenericEntry nt_elbowUPSpeed;
   private GenericEntry nt_elbowDOWNSpeed;
 
   private double m_percentage;
   private double m_degrees;
-  private boolean m_autoElbowEnabled = false;
   private double feedforward;
   private double pidOutput;
   private double target_speed;  // fom -1.0 to 1.0
@@ -80,13 +65,7 @@ public class ArmSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    
-    /*if(m_autoElbowEnabled){
-      moveArmToDesiredAngle();
-    };*/
-    
     checkElbowSoftLimits();
-
   }
 
   /*
@@ -107,7 +86,6 @@ public class ArmSubsystem extends SubsystemBase {
     boolean limitHit = true;
     if (degrees < Constants.ArmConstants.kMaxElbowAngle){
       limitHit = false;
-      //System.out.println("lower limit exceed:"+degrees);
     }
     return limitHit;
   }
@@ -117,7 +95,6 @@ public class ArmSubsystem extends SubsystemBase {
     boolean limitHit = true;
     if (degrees > Constants.ArmConstants.kMinElbowAngle){
       limitHit = false;
-      //System.out.println("lower limit exceed:"+degrees);
     }
     return limitHit;
   }
@@ -131,6 +108,23 @@ public class ArmSubsystem extends SubsystemBase {
       return m_elbow.get() < 0;
     }  
 
+    /*
+ * Manually drive the elbow up or down.  When stopped, it will use the control 
+ * loop to hold that new position
+ */
+
+  public void elbowUp() {
+    setElbowSpeed(getElbowUPSpeed());
+ 
+}
+
+public void elbowDown() {
+    setElbowSpeed(getElbowDOWNSpeed());
+}
+
+private void elbowHold(){
+  setElbowSpeed(Constants.ArmConstants.kElbowHoldSpeed);
+}
 
   public Command manualElbowUp(){
     return this.startEnd(
@@ -205,7 +199,6 @@ public class ArmSubsystem extends SubsystemBase {
     
   }
 
-
   protected boolean elbowAtDesiredAngle() {
     // see if we're "close enough" to the target desired angle
     if (Math.abs(getArmAngle() - m_elbowDesiredAngleDeg) <= Constants.ArmConstants.kElbowAngleToleranceDeg) {
@@ -227,43 +220,13 @@ public class ArmSubsystem extends SubsystemBase {
 
     pidOutput = 0.0;
       pidOutput = m_elbowPIDController.calculate(getArmAngle());
-     
-    if (target_speed > 0.0) target_speed = getElbowUPSpeed();
-    if (target_speed < 0.0) target_speed = getElbowDOWNSpeed();
     
-    //limit max speed at point of applying to motor.
-    target_speed = Math.min(target_speed,pidOutput);
+    target_speed = MathUtil.clamp(pidOutput, getElbowUPSpeed(), getElbowDOWNSpeed());
 
     setElbowSpeed(target_speed);
   }
 
-  // use PID positioning to bump arm up/down
-  public void bumpArmUp() {
-    setArmAngle(m_elbowDesiredAngleDeg + ArmConstants.kArmBumpIncrementDeg);
-  }
 
-  public void bumpArmDown() {
-    setArmAngle(m_elbowDesiredAngleDeg - ArmConstants.kArmBumpIncrementDeg);
-  }
-
-/*
- * Manually drive the elbow up or down.  When stopped, it will use the control 
- * loop to hold that new position
- */
-
-  public void elbowUp() {
-      setElbowSpeed(getElbowUPSpeed());
-   
-  }
-
-  public void elbowDown() {
-      setElbowSpeed(getElbowDOWNSpeed());
-  }
-
-  private void elbowHold(){
-    // I've bypassed the setElbowSpeed method to avoid all the calculations there. This can use that method if we test it to work.
-    m_elbow.set(Constants.ArmConstants.kElbowHoldSpeed);
-  }
 
   public void stopElbow() {
     m_elbow.set(0);
@@ -286,24 +249,6 @@ public class ArmSubsystem extends SubsystemBase {
     return d;
   }
 
-  private void setupShuffleboard() {
-
-    ShuffleboardTab armTab = Shuffleboard.getTab("Arm");
-
-    nt_elbowUPSpeed = armTab.addPersistent("Elbow UP speed", Constants.ArmConstants.kElbowUpSpeed)
-        .withSize(3, 1)
-        .withWidget(BuiltInWidgets.kNumberSlider)
-        .withProperties(Map.of("min", 0, "max", 1))
-        .getEntry();
-
-    nt_elbowDOWNSpeed = armTab.addPersistent("Elbow DOWN speed", Constants.ArmConstants.kElbowDownSpeed)
-        .withSize(3, 1)
-        .withWidget(BuiltInWidgets.kNumberSlider)
-        .withProperties(Map.of("min", 0, "max", 1))
-        .getEntry();
-
-  }
-
   private void setArmAngle(double desiredAngle) {
     m_elbowDesiredAngleDeg = desiredAngle;
     m_elbowPIDController.setSetpoint(desiredAngle);
@@ -321,30 +266,7 @@ public class ArmSubsystem extends SubsystemBase {
     if (setspeed < 0 && minimumLimitReached()){
       return;
     }*/
-/* 
-    m_autoElbowEnabled = true; // we've requested arm movement, enable it 
-    double dampenedSpeed = setspeed;
-    // dial down the requested speed if we are close to the target angle
-    if (Math.abs(getArmAngle() - m_elbowDesiredAngleDeg) <= (3.0*Constants.ArmConstants.kElbowAngleToleranceDeg)) {
-      dampenedSpeed = 0.5*setspeed;
-    }
-    double degrees = this.getArmAngle();
-    if ((degrees < Constants.ArmConstants.kMaxElbowAngle) &&
-        (degrees > Constants.ArmConstants.kMinElbowAngle)) {
-      // This is ok - not in limit so set the speed regardless of direction
-      m_elbow.set(dampenedSpeed);
-    } else if ((degrees > Constants.ArmConstants.kMaxElbowAngle) &&
-        (dampenedSpeed < 0.0)) {
-      // this is OK. Currently exceeding max limit but it's OK to move in direction
-      // away from the limit
-      m_elbow.set(dampenedSpeed);
-    } else if ((degrees < Constants.ArmConstants.kMinElbowAngle) &&
-        (dampenedSpeed > 0.0)) {
-      // this is OK. Currently exceeding min limit but it's OK to move in direction
-      // away from the limit
-      m_elbow.set(dampenedSpeed);
-    }
-      */
+
       m_elbow.set(setspeed);
   }
 
@@ -367,6 +289,23 @@ public class ArmSubsystem extends SubsystemBase {
     return -1.0*elbowDownSpeed;
   } 
 
+  private void setupShuffleboard() {
+
+    ShuffleboardTab armTab = Shuffleboard.getTab("Arm");
+
+    nt_elbowUPSpeed = armTab.addPersistent("Elbow UP speed", Constants.ArmConstants.kElbowUpSpeed)
+        .withSize(3, 1)
+        .withWidget(BuiltInWidgets.kNumberSlider)
+        .withProperties(Map.of("min", 0, "max", 1))
+        .getEntry();
+
+    nt_elbowDOWNSpeed = armTab.addPersistent("Elbow DOWN speed", Constants.ArmConstants.kElbowDownSpeed)
+        .withSize(3, 1)
+        .withWidget(BuiltInWidgets.kNumberSlider)
+        .withProperties(Map.of("min", 0, "max", 1))
+        .getEntry();
+
+  }
 
   public void initSendable(SendableBuilder builder){
     builder.addDoubleProperty("Raw Absolute Encoder", ()-> m_percentage, null);
--- src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -42,16 +42,6 @@ public class WristSubsystem extends SubsystemBase {
     setupShuffleboard();
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a
-   * digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
 
   @Override
   public void periodic() {
