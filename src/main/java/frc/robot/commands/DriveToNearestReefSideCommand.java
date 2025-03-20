69a9a5e110cf1725b0b38295646b098642e3f88d
Cliff Stockdill <cstockdill@yahoo.com>
2025-03-20 14:16:20 -0600

    update

--- src/main/java/frc/robot/commands/DriveToNearestReefSideCommand.java
+++ src/main/java/frc/robot/commands/DriveToNearestReefSideCommand.java
@@ -11,17 +11,14 @@ import java.util.Map;
 import java.util.Optional;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.util.AprilTagPositions;
 import frc.robot.subsystems.SwerveSubsystem;
 
